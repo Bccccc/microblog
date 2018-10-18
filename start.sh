8b692c7fc9ef119838dc93df981ec1b040256799
Baoci Li <bc@BcdeMacBook-Pro.local>
2018-10-17 21:42:05 -0400

    404 500 page email service

--- start.sh
+++ start.sh
@@ -1,2 +1,10 @@
 export FLASK_APP=microblog.py
+export FLASK_DEBUG=1
+
+export MAIL_SERVER=smtp.googlemail.com
+export MAIL_PORT=587
+export MAIL_USE_TLS=1
+export MAIL_USERNAME=bli14.microblog@gmail.com
+export MAIL_PASSWORD=Libaoci123
+
 flask run
